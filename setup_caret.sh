756e14e08d56dbc8099de1141fd4984b4c318aa3
Takayuki AKAMINE <38586589+takam5f2@users.noreply.github.com>
2023-01-30 08:38:12 +0900

    chore(ansible): add query to install packages (#72)
    * chore(ansible): add query to install packages
    
    Signed-off-by: Takayuki AKAMINE <takayuki.akamine@tier4.jp>
    
    * ci(pre-commit): autofix
    
    * chore(ansible): refactored distribution checking
    
    Signed-off-by: Takayuki AKAMINE <takayuki.akamine@tier4.jp>
    
    * chore(ansible): suppress some warnings
    
    Signed-off-by: Takayuki AKAMINE <takayuki.akamine@tier4.jp>
    
    * chore(ansible): suppress the warning message.
    
    Signed-off-by: Takayuki AKAMINE <takayuki.akamine@tier4.jp>
    
    * chore(ansible): removed ros2 role
    
    Signed-off-by: Takayuki AKAMINE <takayuki.akamine@tier4.jp>
    
    ---------
    
    Signed-off-by: Takayuki AKAMINE <takayuki.akamine@tier4.jp>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- setup_caret.sh
+++ setup_caret.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
-DISTRIB_RELEASE=$(grep -e "DISTRIB_RELEASE=" /etc/lsb-release | sed -e "s/DISTRIB_RELEASE=//g")
-TOOL_UBUNTU_VERSION=$(grep -e 'ubuntu_version' ./ansible/playbook.yml | sed -E 's/[^0-9\.]*([0-9\.]*).*/\1/g')
+set -Ceu
 
-if [[ ! $DISTRIB_RELEASE =~ $TOOL_UBUNTU_VERSION ]]; then
-    echo "This script is served for Ubuntu ${DISTRIB_RELEASE}"
-    echo "Run this script on Ubuntu ${TOOL_UBUNTU_VERSION}"
+SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
+
+function show_usage() {
+    echo "${0} executes setup for CARET."
+    echo "Options "
+    echo "    -h or --help: show help message"
+    echo "    -c or --no-interactive"
+    echo "    -n or --no-package-install"
     exit 0
-fi
+}
+
+# parse command options.
+OPT=$(getopt -o nch -l no-interactive,no-package-install,help -- "$@")
+
+eval set -- "$OPT"
 
 # Parse args
 noninteractive=0
-while getopts c OPT; do
-    case $OPT in
-    "c")
-        noninteractive=1
-        echo "run setup with a noninteractive mode"
-        ;;
-    "*") ;;
+package_install=1
 
+while true; do
+    case $1 in
+    -h | --help)
+        show_usage
+        ;;
+    -c | --no-interactive)
+        noninteractive=1
+        shift
+        ;;
+    -n | --no-package-install)
+        package_install=0
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
     esac
 done
 
-# Confirm to start installation
+# Confirm whether to start installation
 if [ $noninteractive -eq 0 ]; then
     # Prevent root execution in interactive mode
     if [ "$(id -u)" -eq 0 ]; then
@@ -31,19 +50,16 @@ if [ $noninteractive -eq 0 ]; then
         exit 1
     fi
 
-    echo "Setting up the build environment take up to 30 minutes."
+    echo "Setting up the build environment take up to 20 minutes."
     read -rp ">  Are you sure you want to run setup? [y/N] " answer
-else
-    answer="y"
-fi
 
-# Use "Super Cow Powers"!
-if [[ $answer == [cC] ]]; then
-    if ! (command -v cowsay >/dev/null 2>&1); then
-        sudo apt-get install -y cowsay
+    # Check whether to cancel
+    if ! [[ ${answer:0:1} =~ y|Y ]]; then
+        echo -e "\e[33mCancelled.\e[0m"
+        exit 1
     fi
-
-    answer="y"
+else
+    echo -e "\e[36mRun the setup in non-interactive mode.\e[m"
 fi
 
 # Install sudo
@@ -59,32 +75,28 @@ if ! (command -v pip3 >/dev/null 2>&1); then
 fi
 
 # Run ansible if confirmed
-case $answer in
-[yY]*)
-    # Install ansible
-    ansible_version=$(pip3 list | grep -oP "^ansible\s+\K([0-9]+)" || true)
-    if [ "$ansible_version" != "5" ]; then
-        sudo apt-get -y purge ansible
-        sudo pip3 install -U "ansible==5.*"
-    fi
-    # Set options
-    if [ $noninteractive -eq 0 ]; then
-        options=("--ask-become-pass")
-    else
-        options=("--extra-vars" "yn_gpu=y")
-    fi
+# Install ansible
+# TODO: To update Ansible 6.*
+ansible_version=$(pip3 list | grep -oP "^ansible\s+\K([0-9]+)" || true)
+if [ "$ansible_version" != "5" ]; then
+    sudo apt-get -y purge ansible
+    sudo pip3 install -U "ansible==5.*"
+fi
 
-    # Run ansible
-    if ansible-playbook "$SCRIPT_DIR/ansible/playbook.yml" -e WORKSPACE_ROOT="$SCRIPT_DIR" "${options[@]}"; then
-        echo -e "\e[32mCompleted.\e[0m"
-        exit 0
-    else
-        echo -e "\e[31mFailed.\e[0m"
-        exit 1
-    fi
-    ;;
-*)
-    echo -e "\e[33mCancelled.\e[0m"
+# Set options
+if [ $noninteractive -eq 0 ]; then
+    options=("--ask-become-pass")
+fi
+
+if [ $package_install -eq 0 ]; then
+    options=("--extra-vars" "package_install=n")
+fi
+
+# Run ansible
+if ansible-playbook "$SCRIPT_DIR/ansible/playbook.yml" -e WORKSPACE_ROOT="$SCRIPT_DIR" "${options[@]}"; then
+    echo -e "\e[32mCompleted.\e[0m"
+    exit 0
+else
+    echo -e "\e[31mFailed.\e[0m"
     exit 1
-    ;;
-esac
+fi
